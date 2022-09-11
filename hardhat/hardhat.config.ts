df0e43057dfcb9a61556310dcf953c1f1bcb51d3
ahsenrz <ahsenriaz2001@gmail.com>
2022-09-11 17:00:25 +0500

    Updated solidity version

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,7 +2,11 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: "0.8.17",
+  paths:{
+    artifacts:'../frontend/ABIs'
+  }
+
 };
 
 export default config;
